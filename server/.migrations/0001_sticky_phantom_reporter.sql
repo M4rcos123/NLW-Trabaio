c36163bea9fbb19e8a1b1f3afa7b70fd6837d7f2
M4rcos123 <marcos.junior@aluno.faeterj-brm.faetec.rj.gov.br>
2024-09-17 09:41:42 -0300

    quinto commit

--- server/.migrations/0001_sticky_phantom_reporter.sql
+++ server/.migrations/0001_sticky_phantom_reporter.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "goal_completions" (
 	"id" text PRIMARY KEY NOT NULL,
 	"goal_id" text NOT NULL,
-	"create_at" timestamp with time zone DEFAULT now() NOT NULL
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 DO $$ BEGIN
